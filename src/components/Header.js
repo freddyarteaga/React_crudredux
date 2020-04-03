4d4d3bbc47a3ae9b68f89217b446f62aacaabd43
Freddy <freddy.artea@gmail.com>
2020-04-02 20:15:29 -0500

    creating the header

---
+++ src/components/Header.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Header = () => {
+    return ( 
+        <nav className='navbar navbar-expand-lg navbar-dark bg-primary
+        justify-content-between'>
+            <div className='container'>
+                <h1>CRUD - React, Redux, REST API & Axios</h1>
+            </div>
+
+            <a href='/products/new'
+                className='btn btn-danger new-post d-block d-md-inline-block'
+            >Add Products &#43;</a>
+        </nav>
+     );
+}
+ 
+export default Header;
