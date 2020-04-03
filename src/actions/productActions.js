57eb82b31e82956a25bb840a994f3f372517cca4
Freddy <freddy.artea@gmail.com>
2020-04-03 16:15:30 -0500

    adding new product to state

--- src/actions/productActions.js
+++ src/actions/productActions.js
@@ -6,7 +6,29 @@ import {
 
 // Create new products
 export function createNewProductAction(product) {
-    return () => {
-        console.log(product)
+    return (dispatch) => {
+        dispatch( addProduct() );
+
+        try {
+            dispatch( addProductSuccess(product) )
+        } catch (error) {
+            dispatch( addProductError(true) )
+        }
     }
 }
+
+const addProduct = () => ({
+    type: ADD_PRODUCT,
+    payload: true
+})
+
+// if prodcut save in the BD
+const addProductSuccess = product => ({
+    type: ADD_PRODUCT_SUCCESS,
+    payload: product
+})
+
+// if there was an error
+const addProductError = () => ({
+
+})
